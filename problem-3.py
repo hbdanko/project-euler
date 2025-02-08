f3cec8a6b1654a368fb9ccecfb83297ffe9e454f
Hakan Baran <hbhakanbaran@gmail.com>
2025-02-09 02:57:43 +0300

    largest_prime_factor

--- problem-3.py
+++ problem-3.py
@@ -3,7 +3,4 @@ from sympy import factorint
 def largest_prime_factor(prime):
     return max(factorint(prime))
 
-#print(largest_prime_factor(600851475143))
-
-deneme = factorint(600851475143)
 print(largest_prime_factor(600851475143))
