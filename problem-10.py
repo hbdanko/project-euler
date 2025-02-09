ea6a88693bde63df1f64e2e6fbbf44ccae6ba789
Hakan Baran <hbhakanbaran@gmail.com>
2025-02-09 17:12:11 +0300

    Summation of Primes

--- problem-10.py
+++ problem-10.py
@@ -12,13 +12,13 @@ def prime_generator():
             num += 2
 
 def summation_of_primes(ceiling):
-    total = 2  # Start the sum with 2 (the first prime number)
+    total = 2
     gen = prime_generator()
     
-    prime = next(gen)  # Get the first prime
+    prime = next(gen)
     while prime < ceiling:
         total += prime
-        prime = next(gen)  # Get the next prime
+        prime = next(gen)
     
     return total
 
