424257f0bbc4b78647093e40aee2edb041a59ff7
Hakan Baran <hbhakanbaran@gmail.com>
2025-02-09 16:07:45 +0300

    Sum Square Difference

--- problem-6.py
+++ problem-6.py
@@ -1,12 +1,3 @@
-def square_of_sum(ceiling):
-    return sum(list(range(1, ceiling + 1))) ** 2
-
-def sum_of_squares(ceiling):
-    return sum([num ** 2 for num in range(1, ceiling + 1)])
-
-print(square_of_sum(10))
-print(sum_of_squares(10))
-
 def sum_square_difference(ceiling):
     square_of_sum = sum(list(range(1, ceiling + 1))) ** 2
     sum_of_squares = sum([num ** 2 for num in range(1, ceiling + 1)])
